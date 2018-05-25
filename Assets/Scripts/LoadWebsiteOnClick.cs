da34ef5bc5e4d14768c6b6688d162cbf4da48c50
vivekchdry <vschoudhary193@gmail.com>
2018-05-25 13:08:43 +0530

    First Unity 3D Project

---
+++ Assets/Scripts/LoadWebsiteOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadWebsiteOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	public void LinkWeb () {
+		Application.OpenURL("http://www.geu.ac.in/");
+	}
+
+	void Update(){
+	}
+}
